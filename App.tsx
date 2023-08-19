efdac8b0e69fb0343b203066c13c9b5ddd3a97f4
cavid-aliyev <javidalyevv@gmail.com>
2023-08-19 09:58:59 +0400

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Salaaam</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
